8ecf11009cf12ae43e4c0481de49d163e412b3ae
BoNaomiHeins <bonheins@gmail.com>
2021-02-10 17:33:12 +0100

    message

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express =require('express');
 const app = express();
-const port = 8000;
+const port = 8000;  
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'static/public')));
 
 app.get("/", (req, res)=> {
   res.sendFile(__dirname + '/static/public/index.html')
